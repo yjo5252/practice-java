225e4fc0c5764b4751c7442c8370728b75fe4a1d
Yeeun Grace Jo <41981471+yjo5252@users.noreply.github.com>
2020-01-31 03:16:19 +0900

    Add files via upload

---
+++ epper17/epper17_1.java
@@ -0,0 +1,32 @@
+﻿// epper 17_1.java
+
+/*  입력: 양의 정수 두개
+    출력: 최대공약수 출력
+*/
+
+import java.util.Scanner;
+public class EPPER1 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String input;
+    int a, b;
+    int big, small;
+    a = sc.nextInt();
+    b = sc.nextInt();
+
+    if (a > b) {
+      big = a ;  small = b;
+    }else{
+      big = b;   small = a;
+    }
+
+    int mod = big % small;
+
+    while(mod >0){
+      big = small;
+      small = mod;
+      mod = big%small;
+    }
+    System.out.println(small);
+  }
+}
---
+++ epper17/epper17_3.java
@@ -0,0 +1,48 @@
+﻿// epper 17_3.java
+
+/*  입력: 숫자 n개 (stack 자료구조)
+    출력: palindrome 이면 yes, 아니면 no
+*/
+
+import java.util.Scanner;
+public class EPPER3 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    boolean res = true;
+    Stack sta = new Stack();
+    String input;
+    int len, end, start;
+    char[] Arr;
+    char temp;
+
+    input = sc.next();
+    len = n.length();
+    Arr = n.toCharArray();
+
+    //choose stat & end
+    if (len % 2 ==0){
+      end = len/2-1;
+      start = len/2;
+    }else{
+      end = len/2-1;
+      start = len/2+1;
+    }
+
+    //push to stack
+    for (int i=0; i<=end; i++){
+      sta.push(Arr[i]);
+    }
+
+    for (int i=start; i<len; i++){
+      if(!sta.isEmpty()){
+        temp = (char) sta.peek();
+        sta.pop();
+        if(temp!=Arr[i]){
+          res = false;
+        }
+      }
+    }
+    if(res==false) System.out.println("no");
+    else System.out.println("yes");
+  }
+}
