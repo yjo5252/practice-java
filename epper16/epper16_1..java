8dc448ad9af84757b958d96a9dd5587f6f233454
Yeeun Grace Jo <41981471+yjo5252@users.noreply.github.com>
2020-02-02 23:44:08 +0900

    Add files via upload

---
+++ epper16/epper16_1..java
@@ -0,0 +1,16 @@
+﻿// epper 16_1.java
+
+/*  입력: 양의 정수 n을 입력
+  출력: 1 + 1/2 + 1/3 + ....+1//n을 소수 둘째 자리까지 출력
+*/
+import java.util.*;
+
+public class EPPER1 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(Ssytem.in);
+    int n = sc.nextInt();
+    double sum = 0;
+    for(int i=1; i<= n; i++)sum+= 1/(double)i;
+    System.out.printf("%.2f\n", sum);
+  }
+}
---
+++ epper16/epper16_2.java
@@ -0,0 +1,18 @@
+﻿// epper 16_2.java
+
+/*  입력: 10개의 정수를 공백으로 구분 입력
+  출력: 최대값 최소값 출력
+  */
+import java.util.Arrays;
+import java.util.*;
+
+public class EPPER2 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(Ssytem.in);
+    int arr[] = new int[10];
+    for(int i=0; i< 10; i++) arr[i] = sc.nextInt();
+    Arrays.sort(arr);
+    System.out.prinln("최대값:"+arr[9]);
+    System.out.println("최소값:"+arr[0]);
+  }
+}
