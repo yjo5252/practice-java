cef48ac2d8103ae51bf70dbc2f54529387c1b434
Yeeun Grace Jo <41981471+yjo5252@users.noreply.github.com>
2020-02-04 07:45:54 +0900

    Add files via upload

---
+++ epper16/epper16_3.java
@@ -0,0 +1,20 @@
+﻿// epper 16_3.java
+
+/*  입력: 양의 정수 n
+  출력: 회문 여부 출력
+  */
+
+import java.util.*;
+
+public class EPPER3 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(Ssytem.in);
+    String str1 = sc.next();
+    StringBuffer rev = new StringBuffer(str1);
+    rev = rev.reverse();
+    String str2 = new String(rev);
+
+    if(str1.equals(str2)) System.out.println("T");
+    else System.out.println("F");
+  }
+}
---
+++ epper16/epper16_4.java
@@ -0,0 +1,23 @@
+﻿// epper 16_4.java
+
+/*  입력: 자연수  n
+  출력: n의 이진수 
+  */
+
+import java.util.*;
+
+public class EPPER4 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(Ssytem.in);
+    int n = sc.nextInt();
+    printBinary(n);
+    System.out.println(n);
+  }
+  public static void printBinary(int n){
+    if(n==0) return;
+    else{
+      printBinary(n/2);
+      System.our.print(n%2);
+    }
+  }
+}
