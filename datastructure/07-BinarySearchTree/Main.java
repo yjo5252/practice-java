8caadf76e1da9300b1781946f072b928f2f322b7
Yeeun Grace Jo <41981471+yjo5252@users.noreply.github.com>
2020-02-19 23:09:45 +0900

    BinarySearchTree

---
+++ datastructure/07-BinarySearchTree/Main.java
@@ -0,0 +1,19 @@
+﻿public class Main {
+  public static void main (String[] args){
+    Tree TM = new Tree();
+    TM.add(10, "cho");
+    TM.add(20, "KIM");
+    TM.add(30, "minho");
+    TM.add(40, "JYeon");
+
+    // (1)
+    System.out.println('Data Search and Get... ');
+    Object Temp = TM.get(20);
+    System.out.println(Temp);
+
+    // (2)
+    System.out.println('Data Search and Get... ');
+    Object Temp = TM.get(40);
+    System.out.println(Temp);
+  }
+}
---
+++ datastructure/07-BinarySearchTree/TreeNode.java
@@ -0,0 +1,32 @@
+﻿public class TreeNode{
+  private int itsKey;
+  private Object itsValue;
+  private TreeNode nodes[] = new TreeNode[2];
+  public TreeNode (int key, Object Value) {
+    itsKey = key;
+    itsValue = value;
+    System.out.println("start TreeMapNode");
+  }
+
+  public Object find(int key){
+    if (key == itskey){
+      return itsValue;
+    }
+    return findSub (select SubNode (key), key);
+  private Object findSub (int node, int key){
+    return nodes [node] == null? null: nodes[node].find(key);
+  }
+  private int selectSubNode(int key){
+    return (key < itsKey)?0:1;
+  }
+  public void add(int key, Object Value){
+    if (key == itsKey) itsValue = value;
+    else SubNode (selectSubNode (key), key, value);
+  }
+  private void SubNode(int node, int key, Object value){
+    if (nodes[node] == null)
+        nodes[node] = new TreeNode(key, value);
+    else
+        nodes[node].add(key, value);
+  }
+}
