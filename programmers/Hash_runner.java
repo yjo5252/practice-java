449ec8c1d6512d3df2dac481f70a9c708726a741
Yeeun Grace Jo <41981471+yjo5252@users.noreply.github.com>
2020-03-13 00:38:45 +0900

    Update Hash_runner.java

--- programmers/Hash_runner.java
+++ programmers/Hash_runner.java
@@ -1,6 +1,6 @@
 //  문제: 완주하지 못한 선수 https://programmers.co.kr/learn/courses/30/lessons/42576 
 // 자바 Hash 사용
-import java util.HashMap;
+import java.util.HashMap;
 
 public class Solution {
   public String solution (String[] participant, String[] completion){
